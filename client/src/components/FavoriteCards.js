1a1b89cafda17008836070558393940dbe7a3aa6
David Luu <dluu.audeo@gmail.com>
2022-04-20 11:54:41 -0700

    basic set up

---
+++ client/src/components/FavoriteCards.js
@@ -0,0 +1,103 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { useMutation, useQuery } from "@apollo/client";
+import { ME } from "../util/queries";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { REMOVE_FAVORITE_RESTAURANT } from "../util/mutations";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const FavoriteCards = () => {
+  const { loading, data } = useQuery(ME);
+  const restaurants = data?.me.favoriteRestaurants || [];
+
+  const [removeFavorite] = useMutation(REMOVE_FAVORITE_RESTAURANT, {
+    // refetchQueries so once removeFavorite gets called the favorite restaurants will update from ME query
+    refetchQueries: [ME],
+  });
+
+  const handleRemove = (restaurantId) => {
+    removeFavorite({ variables: { restaurantId } });
+  };
+
+  if (restaurants.length === 0) {
+    return (
+      <div
+        sx={{
+          maxWidth: 345,
+          mx: "auto",
+          marginBottom: "50px",
+          backgroundColor: "transparent",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "center",
+            fontSize: "45px",
+            marginTop: "30px",
+            color: "white",
+            textShadow: "0px 0px 10px darkred",
+          }}
+        >
+          Oh no! <br/>
+          😢
+        </h1>
+        <h3
+          style={{
+            textAlign: "center",
+            marginTop: "20px",
+            fontSize: "25px",
+            color: "white",
+            textShadow: "0px 0px 10px darkred",
+          }}
+        >
+          You don't have <br />
+          any favorite restaurants! <br />< br/>
+
+          Please go add some to your list!
+        </h3>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {loading && <p>loading restaurants...</p>}
+      {restaurants.map((restaurant) => (
+        <Card
+          key={restaurant._id}
+          sx={{ maxWidth: 345, my: "30px", mx: "auto" }}
+        >
+          <Link to={`/restaurants/${restaurant._id}`}>
+            <CardMedia
+              component="img"
+              height="200"
+              image={restaurant.restaurantImg}
+              alt="restaurant"
+            />
+          </Link>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {restaurant.restaurantName}
+            </Typography>
+            <Typography variant="body2" color="text.primary">
+              {restaurant.foodType}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {restaurant.address} {restaurant.zipCode}
+            </Typography>
+            <Typography variant="body2" color="text.primary">
+              {restaurant.happyHours}
+            </Typography>
+            <DeleteIcon onClick={() => handleRemove(restaurant._id)}>Unlike</DeleteIcon>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+export default FavoriteCards;
---
+++ client/src/components/RestaurantCards.js
@@ -0,0 +1,174 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { useMutation, useQuery } from "@apollo/client";
+import { QUERY_BY_ZIPCODE } from "../util/queries";
+import { useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Grid from "@mui/material/Button";
+import { ADD_FAVORITE_RESTAURANT } from "../util/mutations";
+import { useMediaQuery } from "@mui/material";
+import "../../src/index.css";
+
+const Cards = () => {
+  const [searchParams] = useSearchParams();
+  const zipCode = parseInt(searchParams.get("zipcode"));
+
+  const { error, loading, data } = useQuery(QUERY_BY_ZIPCODE, {
+    variables: { zipCode },
+  });
+  const restaurants = data?.restaurantsByZipcode || [];
+
+  const featuredRestaurant = restaurants[restaurants.length - 1];
+
+  const [addFavorite, addFavoriteState] = useMutation(ADD_FAVORITE_RESTAURANT);
+
+  const handleFavorite = (restaurantId) => {
+    addFavorite({ variables: { restaurantId } });
+  };
+
+  // display missing data if zipcode is not found
+  if (restaurants.length === 0) {
+    return (
+      <div
+        sx={{
+          maxWidth: 345,
+          mx: "auto",
+          marginBottom: "50px",
+          backgroundColor: "transparent",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "center",
+            fontSize: "45px",
+            marginTop: "30px",
+            color: "white",
+            textShadow: "0px 0px 10px darkred",
+          }}
+        >
+          Oops! <br/>
+          😢
+        </h1>
+        <h3
+          style={{
+            textAlign: "center",
+            marginTop: "20px",
+            fontSize: "25px",
+            color: "white",
+            textShadow: "0px 0px 10px darkred",
+          }}
+        >
+          There aren't any <br />
+          happy hour specials <br />
+          in your selected zipcode. <br/> <br/>
+
+          Please try another zipcode!
+        </h3>
+      </div>
+    );
+  }
+
+  return (
+    <Box>
+    <Box sx={{ flexGrow: 1, borderBottom: 2 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }}></Grid>
+      {loading && <p>loading restaurants...</p>}
+        <Card
+          key={featuredRestaurant._id}
+          sx={{ maxWidth: "50vw", my: "30px", mx: "auto" }}
+        >
+          <Grid
+            xs={12}
+            sx={{ display: "flex", flexDirection: { xs: "column" } }}
+            key={featuredRestaurant._id}
+          >
+            <Link
+              to={`/restaurants/${featuredRestaurant._id}`}
+              style={{ width: "100%", textDecoration: "none" }}
+            >
+              <CardMedia
+                component="img"
+                image={featuredRestaurant.restaurantImg}
+                alt="featuredRestaurant"
+              />
+            <CardContent>
+            <Typography gutterBottom component="div" fontSize="20px" color="red">
+                Featured Restaurant
+              </Typography>
+              <Typography gutterBottom component="div" color="blue" fontWeight="bold" fontSize="20px">
+                {featuredRestaurant.restaurantName}
+              </Typography>
+
+              <Typography variant="body2" color="text.secondary">
+                {featuredRestaurant.address} <br />
+                {featuredRestaurant.city}, {featuredRestaurant.state}
+                <br />
+                {featuredRestaurant.zipCode}
+              </Typography>
+
+              <Typography variant="body2" color="text.primary">
+                {featuredRestaurant.happyHours}
+              </Typography>
+              {/* <Button onClick={() => handleFavorite(restaurant._id)}>
+                Favorite
+              </Button> */}
+            </CardContent>
+              </Link>
+          </Grid>
+        </Card>
+    </Box>
+    <Box sx={{ flexGrow: 1 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }}></Grid>
+      {loading && <p>loading restaurants...</p>}
+      {restaurants.map((restaurant) => (
+        <Card
+          key={restaurant._id}
+          sx={{ maxWidth: "50vw", my: "30px", mx: "auto" }}
+        >
+          <Grid
+            xs={12}
+            sx={{ display: "flex", flexDirection: { xs: "column" } }}
+            key={restaurant._id}
+          >
+            <Link
+              to={`/restaurants/${restaurant._id}`}
+              style={{ width: "100%", textDecoration: "none" }}
+            >
+              <CardMedia
+                component="img"
+                image={restaurant.restaurantImg}
+                alt="restaurant"
+              />
+            <CardContent>
+              <Typography gutterBottom component="div" color="blue" fontWeight="bold" fontSize="20px">
+                {restaurant.restaurantName}
+              </Typography>
+
+              <Typography variant="body2" color="text.secondary">
+                {restaurant.address} <br />
+                {restaurant.city}, {restaurant.state}
+                <br />
+                {restaurant.zipCode}
+              </Typography>
+
+              <Typography variant="body2" color="text.primary">
+                {restaurant.happyHours}
+              </Typography>
+              {/* <Button onClick={() => handleFavorite(restaurant._id)}>
+                Favorite
+              </Button> */}
+            </CardContent>
+              </Link>
+          </Grid>
+        </Card>
+      ))}
+    </Box>
+    </Box>
+  );
+};
+
+export default Cards;
